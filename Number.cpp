35f4bbf86b57fc064d85a77bcc06db0f88abcb70
Hardik <hardikagarwal2001@gmail.com>
2020-09-20 00:51:10 +0530

    rechange

--- Number.cpp
+++ Number.cpp
@@ -2,27 +2,16 @@
 
 using namespace std;
 
-void fun2(int n)
-{
-    if(n>50)
-        cout<<"Number >50";
-    else
-        cout<<"Number <50";
-}
-
-
-/*
 void fun(int n)
 {
     if(n%2)
         cout<<"Odd";
     else cout<<"Even";
-}*/
+}
 
 main()
 {
     int n;
     cin>>n;
-//    fun(n);
-    fun2(n);
+    fun(n);
 }
