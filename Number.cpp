3428a1d5b9fcfb2ffd9741d0152c94c16072c08e
Hardik <hardikagarwal2001@gmail.com>
2020-09-20 00:48:53 +0530

    In dev

--- Number.cpp
+++ Number.cpp
@@ -2,16 +2,27 @@
 
 using namespace std;
 
+void fun2(int n)
+{
+    if(n>50)
+        cout<<"Number >50";
+    else
+        cout<<"Number <50";
+}
+
+
+/*
 void fun(int n)
 {
     if(n%2)
         cout<<"Odd";
     else cout<<"Even";
-}
+}*/
 
 main()
 {
     int n;
     cin>>n;
-    fun(n);
+//    fun(n);
+    fun2(n);
 }
